6638ca363589ed29524711d898979d5b1899e7cf
dwayne <39229311+xdjx19@users.noreply.github.com>
2025-04-08 17:21:35 +1000

    Update run_all_searches.bat
    fixed up bat file

--- run_all_searches.bat
+++ run_all_searches.bat
@@ -4,25 +4,22 @@ REM This batch script runs all search methods for a given map file.
 SET FILENAME=PathFinder-test.txt
 
 echo Running BFS...
-python search.py %FILENAME% bfs
+python search.py PathFinder-test.txt bfs
 
 echo Running DFS...
-python search.py %FILENAME% dfs
+python search.py PathFinder-test.txt dfs
+
+echo Running IDDFS...
+python search.py PathFinder-test.txt iddfs
 
 echo Running UCS...
-python search.py %FILENAME% ucs
+python search.py PathFinder-test.txt ucs
 
 echo Running GBFS...
-python search.py %FILENAME% gbfs
+python search.py PathFinder-test.txt gbfs
 
 echo Running A*...
-python search.py %FILENAME% as
-
-echo Running Custom Search 1 (UCS)...
-python search.py %FILENAME% cus1
-
-echo Running Custom Search 2 (IDDFS)...
-python search.py %FILENAME% cus2
+python search.py PathFinder-test.txt as
 
 echo All methods completed.
 pause
