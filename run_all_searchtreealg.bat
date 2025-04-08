d9d5e73a12ff3a8a182aafeccc915915e90398f6
dwayne <39229311+xdjx19@users.noreply.github.com>
2025-04-08 14:30:42 +1000

    Create run_all_searchtreealg.bat
    created framework. need to update and add

---
+++ run_all_searchtreealg.bat
@@ -0,0 +1,28 @@
+@echo off
+REM This batch script runs all search methods for a given map file.
+
+SET FILENAME=map1.txt
+
+echo Running BFS...
+python search.py %FILENAME% bfs
+
+echo Running DFS...
+python search.py %FILENAME% dfs
+
+echo Running UCS...
+python search.py %FILENAME% ucs
+
+echo Running GBFS...
+python search.py %FILENAME% gbfs
+
+echo Running A*...
+python search.py %FILENAME% as
+
+echo Running Custom Search 1 (UCS)...
+python search.py %FILENAME% cus1
+
+echo Running Custom Search 2 (IDDFS)...
+python search.py %FILENAME% cus2
+
+echo All methods completed.
+pause
